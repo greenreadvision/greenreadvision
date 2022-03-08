c3d510b1aa850b38a45bcf1c186e2cee2516fa85
JasonYin <zx99519567@gmail.com>
2022-03-08 18:11:16 +0800

    倉儲頁面

---
+++ resources/views/pm/reserve/indexReserve.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+
+
+
+@stop
+
+@section('script')
+<script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
+<script type="text/javascript" src="https://unpkg.com/xlsx@0.14.0/dist/xlsx.full.min.js"></script>
+
+@stop
