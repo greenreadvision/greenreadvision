3912fce8b1ac76ad8970e0eee8eed2053343481f
JasonYin <zx99519567@gmail.com>
2022-03-08 17:23:27 +0800

    update Hulk點選方式

--- resources/views/pm/hulk/indexHulk.blade.php
+++ resources/views/pm/hulk/indexHulk.blade.php
@@ -9,17 +9,19 @@
                 <h5 class="modal-title">性別</h5>
             </div>
             <div class="modal-body">
-                <div class="form-group row">
+                <div class="form-group row justify-content-center" >
                     <div class="row">
-                        <input type="button" class="btn btn-primary" id="male" value="男" onclick="changeWindows('sex')">
-                        <input type="button" class="btn btn-primary" id="female" value="女" onclick="changeWindows('sex')">
+
+                            <button type="button" class="btn btn-primary rounded-pill" value="男" onclick="changeWindows('sex','male')">男</button>
+
+                            <button type="button" class="btn btn-primary rounded-pill" value="女" onclick="changeWindows('sex','female')">女</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="col-lg-12" style="position: relative;" id="area" tabindex="-1" role="dialog" aria-labelledby="area" aria-hidden="false">
+<div class="col-lg-12" style="position: relative;" id="area" tabindex="-1" role="dialog" aria-labelledby="area" aria-hidden="false" hidden>
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -29,7 +31,7 @@
                 <div class="form-group row">
                     <div class="row">
                          @foreach($areas as $area)
-                         <input type="button" class="btn btn-primary" id="areaa" value="{{__('customize.'.$area)}}" onclick="changeWindows('area')">
+                         <button type="button" class="btn btn-primary" onclick="changeWindows('area','{{$area}}')">{{__('customize.'.$area)}}</button>
                         @endforeach
                     </div>
                 </div>
@@ -37,7 +39,7 @@
         </div>
     </div>
 </div>
-<div class="col-lg-12" style="position: relative;" id="age" tabindex="-1" role="dialog" aria-labelledby="age" aria-hidden="false">
+<div class="col-lg-12" style="position: relative;" id="age" tabindex="-1" role="dialog" aria-labelledby="age" aria-hidden="false" hidden>
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -47,7 +49,7 @@
                  <div class="form-group row">
                     <div class="row">
                         @foreach($ages as $age)
-                        <input type="button" class="btn btn-primary" id="agee" value="{{__('customize.'.$age)}}" onclick="changeWindows('age')">
+                        <button type="button" class="btn btn-primary" onclick="changeWindows('age','{{$age}}')">{{__('customize.'.$age)}}</button>
                         @endforeach
                     </div>
                 </div>
@@ -55,7 +57,7 @@
         </div>
     </div>
 </div>
-<div class="col-lg-12" style="position: relative;" id="confirm" tabindex="-1" role="dialog" aria-labelledby="confirm" aria-hidden="false">
+<div class="col-lg-12" style="position: relative;" id="confirm" tabindex="-1" role="dialog" aria-labelledby="confirm" aria-hidden="false" hidden>
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -67,20 +69,25 @@
                     @csrf
                     <div class="form-group row">
                         <div class="col">
-                            <select>
-                                <option>test</option>
+                            <select name="select_sex" id="select_sex">
+                                <option id='male' value="male">男</option>
+                                <option id='female' value="female">女</option>
                             </select>
-                            <p id="valueSex"></p>
                         </div>
                         <div class="col">
-                            @foreach($ages as $age)
-                            <input type="button" class="btn btn-primary" id="confirm" value="{{__('customize.'.$age)}}" onclick="changeWindows('age')">
-                            @endforeach
+                            <select name="select_age" id="select_age">
+                                @foreach($ages as $age)
+                                <option id="{{$age}}" value="{{$age}}">{{__('customize.'.$age)}}</option>
+                                @endforeach
+                            </select>
+                            
                         </div>
                         <div class="col">
-                            @foreach($ages as $age)
-                            <input type="button" class="btn btn-primary" id="confirm" value="{{__('customize.'.$age)}}" onclick="changeWindows('age')">
-                            @endforeach
+                            <select  name="select_area" id="select_area">
+                                @foreach($areas as $area)
+                                <option id="{{$area}}" value="{{$area}}">{{__('customize.'.$area)}}</option>
+                                @endforeach
+                            </select>
                         </div>
                     </div>
                 </form>
@@ -96,37 +103,44 @@
 <script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
 
 <script>
+    var sex = ""
+    var age = ""
+    var area = ""
+    
     $(document).ready(function(){
-        var sex = document.getElementById('sex')
-        var area = document.getElementById('area')
-        var age = document.getElementById('age')
-        var confirm = document.getElementById('confirm')
-        area.hidden = true
-        age.hidden = true
-        confirm.hidden = true
+        
     });
 
-    function changeWindows(option){
+    function changeWindows(option,value){
         switch (option){
             case 'sex':
                 document.getElementById('sex').hidden = true;
                 document.getElementById('area').hidden = false;
+                sex = value;
                 break
             case 'area':
                 document.getElementById('area').hidden = true;
                 document.getElementById('age').hidden = false;
+                area = value;
                 break
             case 'age':
                 document.getElementById('age').hidden = true;
+                age = value
                 getResult();
                 document.getElementById('confirm').hidden = false;
+                
             default:
         }
     }
 
     function getResult(){
-        var inputSex = document.getElementById("male").value;
-        document.getElementById("valueSex").innerHTML = inputSex;
+        console.log('sex = ' + sex)
+        console.log('area = ' + area)
+        console.log('age = ' + age)
+        document.getElementById(sex).setAttribute('selected',true);
+        document.getElementById(area).setAttribute('selected',true);
+        document.getElementById(age).setAttribute('selected',true);
+
     }
 </script>
 @stop
